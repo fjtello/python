2f70d2ec6ee11cfbab8bb61b5e0d6af4e3f9e1a5
Fco. Javier Tello <fjtello@gmail.com>
2019-11-14 11:20:43 +0100

    Create RandomForestRegression.py

---
+++ MachineLearning/RandomForestRegression.py
@@ -0,0 +1,7 @@
+# as read from GeeksFromGeeks
+# https://www.geeksforgeeks.org/random-forest-regression-in-python/
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
